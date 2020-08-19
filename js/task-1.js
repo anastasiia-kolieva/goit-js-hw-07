92afa5cad5f8c508172f420f8aef044759f94a05
anastasiia-kolieva <nastya1110@gmail.com>
2020-08-20 00:34:34 +0300

    started make task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const categoriesRef = document.querySelector('#categories').children;
+console.log(`В списке ${categoriesRef.length} категории`);
+
+const titleListRef = document.querySelectorAll('h2');
+
+// const arrayTitleList = Array.from(titleListRef);
+// console.log(arrayTitleList[0]);
+
+// console.log(titleListRef[0]);
+// console.log(titleListRef[1]);
+// console.log(titleListRef[2]);
+
+const listRef = document.querySelectorAll('li.item li').length;
+console.log(listRef);
+
+
+
+
+
+
+
