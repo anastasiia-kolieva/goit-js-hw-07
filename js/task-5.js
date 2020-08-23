a887dbb3e6f1e387bce1c817cfd4a473275d81d0
anastasiia-kolieva <nastya1110@gmail.com>
2020-08-23 17:27:22 +0300

    made task-5

---
+++ js/task-5.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const inputRef = document.querySelector('#name-input');
+const nameOutputRef = document.querySelector('#name-output');
+
+inputRef.addEventListener('input', event=>{
+    nameOutputRef.textContent = event.target.value;
+})
+
