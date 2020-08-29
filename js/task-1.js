95f4fd46c667864c3c8d10c382529d3734160b72
anastasiia-kolieva <nastya1110@gmail.com>
2020-08-29 21:52:07 +0300

    found the way how to console amound of list items

--- js/task-1.js
+++ js/task-1.js
@@ -6,15 +6,8 @@ console.log(`В списке ${categoriesRef.length} категории`);
 const titleListRef = Array.from(document.querySelectorAll("h2"));
 titleListRef.forEach((title) => console.log(`Категория:`, title.textContent));
 
-// Попыталась написать функцию для вывода количества лишек
-// Выводит три одинаковых сообщение о количестве лишек первого списка
-
-const itemListRef = Array.from(document.querySelectorAll(".item"));
-
-const getAmountOflist = () => {
-  const mainListRef = document.querySelector("li.item > ul");
-  const listRef = Array.from(mainListRef.querySelectorAll("li"));
-  return console.log(`Количество элементов: ${listRef.length}`);
-};
-
-itemListRef.forEach((item) => getAmountOflist(itemListRef));
+const mainListRef = document.querySelectorAll("li.item > ul");
+mainListRef.forEach((list) => {
+  const listRef = Array.from(list.querySelectorAll("li"));
+  console.log(`Количество элементов:`, listRef.length);
+});
